e691233966f294350a150267546731d2a005bc0f
Desmond-netw <dsquare200@gmail.com>
2024-12-31 08:43:26 +0200

    change the class name of mySwiper to mySwiper2

--- vendors/js/script.js
+++ vendors/js/script.js
@@ -4,4 +4,26 @@ window.onscroll = () => {
     header.classList.toggle('sticky', window.scrollY > 100);
 };
 /* ========== sticky nav =========== */  
+var swiper = new Swiper(".mySwiper2", {
+    slidesPerView: 1,
+    spaceBetween: 30,
+    loop: true,
+    pagination: {
+      el: ".swiper-pagination",
+      clickable: true,
+    },
+    navigation: {
+      nextEl: ".swiper-button-next",
+      prevEl: ".swiper-button-prev",
+    },
+  });
 
+  var swiper = new Swiper(".mySwiper", {
+    direction: "vertical",
+    slidesPerView: "auto",
+    freeMode: true,
+    scrollbar: {
+      el: ".swiper-scrollbar",
+    },
+    mousewheel: true,
+  });
