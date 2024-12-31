3f4a0542e66194a3613d3f79cbda70266ebe6964
Desmond-netw <dsquare200@gmail.com>
2024-12-31 08:11:03 +0200

    using javascript to crontrol the slider

--- vendors/js/script.js
+++ vendors/js/script.js
@@ -1,6 +1,7 @@
-
+/* ========== sticky nav =========== */  
 window.onscroll = () => {
     let header = document.querySelector('.header');
     header.classList.toggle('sticky', window.scrollY > 100);
 };
+/* ========== sticky nav =========== */  
 
