7ae4a7343d0279aba6d7829082ef809ef2fe38f2
Desmond-netw <dsquare200@gmail.com>
2025-01-01 15:48:13 +0200

    added javascript scrollY effect to display and hide whatsapp button

--- vendors/js/script.js
+++ vendors/js/script.js
@@ -1,9 +1,27 @@
-/* ========== sticky nav =========== */  
+/* ========== sticky  and whatsapp scrol effect =========== */  
 window.onscroll = () => {
     let header = document.querySelector('.header');
-    header.classList.toggle('sticky', window.scrollY > 100);
-};
-/* ========== sticky nav =========== */  
+    const whatsappBtnContainer =document.querySelector('.whatsapp-btn-container');
+    const scrollThreshold = 900;
+
+    // initiate the sticky navbar
+    header.classList.toggle('sticky', window.scrollY > 100)
+
+    //whatsapp container display
+    if(window.scrollY > scrollThreshold) {
+      whatsappBtnContainer.style.display = 'block';
+    }else {
+      whatsappBtnContainer.style.display = 'none';
+    }
+  };
+
+  // ensuring whtaspp is hidden on loading
+  document.addEventListener('DOMContentLoaded', () => {
+    const whatsappBtnContainer=document.querySelector('.whatsapp-btn-container');
+    whatsappBtnContainer.style.display='none';
+  } )
+
+/* ========== Swiper API initilizer =========== */  
 var swiper = new Swiper(".mySwiper2", {
     slidesPerView: 1,
     spaceBetween: 50,
