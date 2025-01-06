69ba1b0ef8fa7cd8f2e3605493ac355b8522af89
Desmond-netw <dsquare200@gmail.com>
2025-01-06 16:59:42 +0200

    change script redirect src

--- vendors/js/redirect_script.js
+++ vendors/js/redirect_script.js
@@ -7,7 +7,7 @@
             body: new FormData(form),
         }).then(() => {
             // Redirect to a thank-you page or show a confirmation message
-            window.location.href = "thank-you.html"; // Replace with your thank-you page URL
+            window.location.href = "../redirects/thank-you.html"; // Replace with your thank-you page URL
         }).catch((error) => {
             console.error('Error submitting the form:', error);
             alert('An error occurred while submitting the form. Please try again.');
