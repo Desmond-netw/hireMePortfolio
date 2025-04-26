ae0ac111b96635371ceec839eb53b3c5ef566dc1
Desmond-netw <dsquare200@gmail.com>
2025-04-26 02:09:51 -0700

    updated packages

--- v2/src/app/page.tsx
+++ v2/src/app/page.tsx
@@ -2,7 +2,7 @@ import Image from "next/image";
 
 export default function Home() {
   return (
-    <div className=" items-center justify-items-center min-h-screen  gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
+    <div className="">
  {/* font-[family-name:var(--font-geist-sans)]d */}
        main content will be here
     </div>
