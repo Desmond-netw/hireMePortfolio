e9fbbd99c4f77704d2ff1d53a7b4c4d7a7daff9a
Desmond-netw <dsquare200@gmail.com>
2025-06-04 20:56:08 +0000

    updated home page

--- v2/src/app/page.tsx
+++ v2/src/app/page.tsx
@@ -2,8 +2,7 @@ import Image from "next/image";
 
 export default function Home() {
   return (
-    <div className="">
- {/* font-[family-name:var(--font-geist-sans)]d */}
+    <div className="w-full">
        main content will be here
     </div>
   );
