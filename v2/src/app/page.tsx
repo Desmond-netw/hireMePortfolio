da3c721ef1b0459508c894dcbbe1d0e1492fb28a
Desmond-netw <dsquare200@gmail.com>
2025-06-05 15:01:35 +0000

    updated home page with drawerlist

--- v2/src/app/page.tsx
+++ v2/src/app/page.tsx
@@ -1,9 +1,19 @@
-import Image from "next/image";
+
+import DrawerList from "./components/DrawerList";
 
 export default function Home() {
   return (
-    <div className="w-full">
-       main content will be here
+    <div className="w-full flex relative">
+      {/* ---side Menu static on large screen */}
+      <aside className="w-[250px] hidden md:flex bg-[#F9F9F8] min-h-screen fixed top-0 left-0 z-10">
+        <DrawerList />
+      </aside>
+
+      {/* ---Main content area with left margin to avoid overlap with fixed sidebar */}
+      <main className="w-full md:ml-[250px] p-4">
+       {/* -----   Other Components Here ------------- */}
+       contents
+      </main>
     </div>
   );
 }
