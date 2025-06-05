f721d6aebc83c0ab549a6a56ec82a2e244bc541c
Desmond-netw <dsquare200@gmail.com>
2025-06-05 15:06:00 +0000

    added container to guid border content on client layout

---
+++ v2/src/app/components/container.tsx
@@ -0,0 +1,19 @@
+"use clients"
+interface ContainerProps {
+    children: React.ReactNode;
+}
+
+const Container:React.FC<ContainerProps> = ({children}) => {
+    return ( <div   className="
+        max-w-[1920px]
+        w-full
+        mx-auto
+        xl:px-20
+        md:px-2
+        px:4
+        ">
+        {children}
+    </div> );
+};
+
+export default Container;
