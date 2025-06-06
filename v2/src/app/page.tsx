dacd17d22d01446a9a8033f928e8bfcd2a5864d3
Desmond-netw <dsquare200@gmail.com>
2025-06-06 18:43:58 +0000

    updated dependency with Fontawesome icons

--- v2/src/app/page.tsx
+++ v2/src/app/page.tsx
@@ -3,16 +3,16 @@ import DrawerList from "./components/DrawerList";
 
 export default function Home() {
   return (
-    <div className="w-full flex relative">
+    <div className="w-full flex ">
       {/* ---side Menu static on large screen */}
-      <aside className="w-[250px] hidden md:flex bg-[#F9F9F8] min-h-screen fixed top-0 left-0 z-10">
+      <aside className="w-[25%] max-w-[250px] hidden md:flex bg-[#F9F9F8] min-h-screen fixed top-0 left-0 z-10">
         <DrawerList />
       </aside>
 
       {/* ---Main content area with left margin to avoid overlap with fixed sidebar */}
-      <main className="w-full md:ml-[250px] p-4">
+      <main className="w-[75%] ml-0 md:ml-[200px] lg:ml-[200px]">
        {/* -----   Other Components Here ------------- */}
-       contents
+       contents con Lorem, ipsum dolor sit amet consectetur adipisicing elit. Amet nam, eveniet quibusdam nobis, porro alias error repellendus obcaecati, esse quisquam similique! Eius, tenetur? Vitae veniam sequi natus, rem voluptatum vero.
       </main>
     </div>
   );
