f51435f9cb99cdfed3e0d18ea9fffa87798b55dc
Desmond-netw <dsquare200@gmail.com>
2025-06-06 18:42:01 +0000

    modify home page layout

--- v2/src/app/ClientLayout.tsx
+++ v2/src/app/ClientLayout.tsx
@@ -4,7 +4,7 @@ import React, { useState } from "react";
 import AppsIcon from "@mui/icons-material/Apps";
 import SideDrawer from "./components/drawer";
 import Container from "./components/container";
-// import Link from "next/link";
+import Link from "next/link";
 
 export default function ClientLayout({
   children,
@@ -22,6 +22,7 @@ export default function ClientLayout({
       <Container>
       {/* Fixed smaill-side-bar-menu */}
       <aside className="w-10 xs:w-6 h-screen fixed top-0 left-0 bg-slate-600 flex flex-col items-center py-15 z-50">
+        <Link href="/" className="p-2 w-full">D.<span className="text-orange-500">D</span></Link>
         <button onClick={() => toggleDrawer(true)} className="md:hidden flex">
           <AppsIcon className="text-white" />
         </button>
